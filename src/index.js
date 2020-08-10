9d9086cd7d1113347c8a95772f05e7789d094db3
Bartosz Pazdur <b.pazdur@onet.pl>
2020-08-10 17:27:00 +0200

    Version 0.0.1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
+const wrapper = document.querySelector('.wrapper')
+
+function createSheet(quantity) {
+    let app = document.createElement('div')
+    app.setAttribute('class', 'app')
+    app.setAttribute('id', 'app')
+
+    wrapper.appendChild(app)
+
+    app = document.querySelector('#app')
+
+    if(typeof quantity === 'number' && quantity > 0) {
+        for(let i = 1; i <= quantity; i++) {
+            let div = document.createElement("div")
+            div.setAttribute('class', 'row')
+            div.textContent = i
+            app.appendChild(div)
+        }
+    } else {
+        throw new Error('Quantity must be integer and must be greater than 0')
+    }
+}
+
+let  quantity = prompt('Ile elementów na płótnie?')
+createSheet(parseInt(quantity))
+
 const rows = document.querySelectorAll('.row')
 const colorPicker = document.querySelector('.picker')
-const resetButton = document.querySelector('.reset')
+const resetButton = document.querySelector('#reset')
 
 let picker = new CP(colorPicker)
 let value = ''
+
+console.log(rows);
 
 resetButton.addEventListener('click', () => {
     for (let index = 0; index < rows.length; index++) {
